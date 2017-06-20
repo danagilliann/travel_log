78946a3260269676c372c93e436beb3309b0430d
danagilliann <danalee96@gmail.com>
2017-06-19 21:54:58 -0400

    css and fonts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,14 +17,8 @@ function Header() {
   return (
     <div className={s.root}>
       <div className={s.container}>
-        <Navigation className={s.nav} />
-        <Link className={s.brand} to="/">
-          <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-          <span className={s.brandTxt}>Your Company</span>
-        </Link>
         <div className={s.banner}>
           <h1 className={s.bannerTitle}>Travel_Log</h1>
-          // <p className={s.bannerDesc}>Complex web apps made easy</p>
         </div>
       </div>
     </div>
