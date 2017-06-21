cbd4965cddb680b8b4e6d86c18a8ad53fe6ac289
danagilliann <danalee96@gmail.com>
2017-06-20 23:10:36 -0400

    inputttt

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,9 +3,24 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Register.scss';
 
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'email',
+      name: 'name',
+      password: 'pass',
+      confirmPassword: 'pass2x',
+      forbiddenWords: ['password', 'user', 'username']
+    };
+  }
+
   render() {
     return (
-      <p>Now with classes</p>
+      <div className={s.registerForm}>
+        <form> {/* add the ff: onSubmit={this.saveAndCountinue} */}
+          <input type="text" value={this.state.email} />
+        </form>
+      </div>
     );
   }
 }
