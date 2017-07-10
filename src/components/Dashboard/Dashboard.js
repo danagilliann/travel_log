b0b393044b0abe55b9b973430a370ae4dab79851
danagilliann <danalee96@gmail.com>
2017-07-09 21:42:54 -0400

    new dashboard component

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Dashboard.scss';
+
+class Dashboard extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello Dashboard</div>
+    );
+  }
+
+}
